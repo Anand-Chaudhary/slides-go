83ece0a72cccd8132c6187a9995103e0e4a05a94
Anand-Chaudhary <your.email@example.com>
2025-09-14 04:20:08 +0000

    Deploy time

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl text-center font-medium">Slides Go</h1>
+      <nav className="flex max-w-7xl mx-auto py-5 justify-between items-center">
+        <div>
+          <h1 className="text-2xl font-bold">Presento</h1>
+        </div>
+        <div className="flex gap-4">
+          <Link href={`/login`}>
+            <Button>Log-In</Button>
+          </Link>
+          <Link href={`/sign-up`}>
+            <Button>Sign-Up</Button></Link>
+        </div>
+      </nav>
     </>
   );
 }
