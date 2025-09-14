98a3fd248c8f9f19e8d38ecdb645134a6662d6af
Anand-Chaudhary <your.email@example.com>
2025-09-14 05:25:37 +0000

    Auth Error Fix

--- frontend/src/services/api/getPPTApi.ts
+++ frontend/src/services/api/getPPTApi.ts
@@ -1,15 +1,25 @@
 import axiosInstance from '../axiosInstance'
 
 export const getPPTApi = {
-    getPPT: async (slug: string)=>{
-        const {data} = await axiosInstance.get(`/ppt/get-ppt?slug=${slug}`);
+    getPPT: async (slug: string) => {
+        const token = localStorage.getItem(`token`)
+        const { data } = await axiosInstance.get(`/ppt/get-ppt?slug=${slug}`, {
+            headers: {
+                Authorization: `bearer ${token}`
+            }
+        });
         return data;
     }
 }
 
 export const getAllPPTApi = {
-    getAllPPT: async ()=>{
-        const {data} = await axiosInstance.get(`/ppt/get-all-ppt`);
+    getAllPPT: async () => {
+        const token = localStorage.getItem(`token`)
+        const { data } = await axiosInstance.get(`/ppt/get-all-ppt`, {
+            headers: {
+                Authorization: `bearer ${token}`
+            }
+        });
         return data;
     }
 }
